43f0c7b370185b70b5740a5e1b74a73131ca0d6f
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-08-30 22:38:12 +0700

    fix: fixing some issues

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="history">
+    <MainHistory />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import NavbarHistory from '@/components/_base/NavbarHistory.vue'
+import MainHistory from '@/components/MainHistory.vue'
+
+export default {
+  name: 'History',
+  components: {
+    MainHistory
+  }
+}
+</script>
